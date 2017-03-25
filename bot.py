d2b4b31b53bd604c6d01f41757dac354ac2430d2
Pmentz <lfants666@gmail.com>
2017-03-25 21:02:24 +0200

    Retweet either greek or global trend

--- bot.py
+++ bot.py
@@ -12,14 +12,16 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)	
 
 def retweet():
-        #globalTrends = api.trends_place(1)
-        greeceTrends = api.trends_place(23424833)
+        if random.randint(1, 2)==1:
+                trends = api.trends_place(1)
+        else:
+                trends = api.trends_place(23424833)
         randomInt = random.randint(1, 10)
-        print (randomInt)
-        hashtag = greeceTrends[0]['trends'][randomInt]['query']
-        print (greeceTrends[0]['trends'][randomInt]['name'])
+        hashtag = trends[0]['trends'][randomInt]['query']
+        print (trends[0]['trends'][randomInt]['name'])
         searchResults = api.search(q=hashtag, rpp=1, result_type='mixed')
-        idToRetweet = searchResults[1].id_str
+        randomInt = random.randint(1, 3)
+        idToRetweet = searchResults[randomInt].id_str
         api.retweet(idToRetweet)
 
 class oleztBot():
