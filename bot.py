3e69b369325d3f3c7d02fd2de4328cb9c862a17f
olezt <lfants666@gmail.com>
2017-03-31 23:43:06 +0300

    Crop black edges from scrambled images

--- bot.py
+++ bot.py
@@ -86,6 +86,15 @@ def scramble(filename):
         c = img.crop(sbox)
         result.paste(c, box)
     result.save('editedImage.png')
+    #Crop black edges
+    img = cv2.imread('editedImage.png')
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+    im2, contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[0]
+    x,y,w,h = cv2.boundingRect(cnt)
+    img = img[y:y+h,x:x+w]
+    cv2.imwrite('editedImage.png',img)
     
 def addMeme(filename, meme):
     img = cv2.imread(filename)
